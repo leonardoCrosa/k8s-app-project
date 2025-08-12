f19bc522f2d88d4b31ccfaae2163c416cb010ac0
Leo <leocrosa18@gmail.com>
2025-08-11 22:09:29 -0300

    Added Helm and K8s providers in preparation for AWS ALB controller configuration in EKS cluster

--- infra/providers.tf
+++ infra/providers.tf
@@ -7,3 +7,22 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
+
+# Configure the k8s Provider for the eks cluster using the token gathered from the datasource
+provider "kubernetes" {
+  host                   = module.eks-cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks-cluster.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+# Configure the helm Provider in the same way as the k8s Provider
+# The embedded kubernetes block makes helm use the same eks access token as the k8s provider
+provider "helm" {
+  kubernetes {
+    host                   = module.eks-cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks-cluster.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+
+
