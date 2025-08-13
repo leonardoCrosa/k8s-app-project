4247569294fae90851e7d68f4f5753438141eb5a
Leo <leocrosa18@gmail.com>
2025-08-13 02:18:25 -0300

    Config aws load balancer controller for EKS cluster via helm on terraform

---
+++ infra/eks-lbc.tf
@@ -0,0 +1,74 @@
+# Trust policy document that allows aws LBC's service account to assume a role
+data "aws_iam_policy_document" "lbc_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [module.eks-cluster.oidc_provider_arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks-cluster.cluster_oidc_issuer_url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks-cluster.cluster_oidc_issuer_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+# The role for LBC to assume
+resource "aws_iam_role" "lbc" {
+  name               = "AmazonEKSLoadBalancerControllerRole"
+  assume_role_policy = data.aws_iam_policy_document.lbc_assume.json
+}
+# The policy with the permissions the LBC will have when assuming the role
+resource "aws_iam_policy" "lbc" {
+  name   = "AWSLoadBalancerControllerIAMPolicy"
+  policy = file("${path.module}/policies/aws-load-balancer-controller.json")
+}
+# Attaches the previous IAM Policy to the IAM Role
+resource "aws_iam_role_policy_attachment" "lbc_attach" {
+  role       = aws_iam_role.lbc.name
+  policy_arn = aws_iam_policy.lbc.arn
+}
+
+
+## Helm install for AWS Load Balancer Ingress Controller Helm Package
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name             = "aws-load-balancer-controller"
+  repository       = "https://aws.github.io/eks-charts"
+  chart            = "aws-load-balancer-controller"
+  namespace        = "kube-system"
+  create_namespace = false
+
+  version = "1.13.4"
+
+  ## Creates a Service Account for LBC controller in my EKS cluster
+  values = [yamlencode({
+    clusterName = module.eks-cluster.cluster_name
+    serviceAccount = {
+      create = true
+      name   = "aws-load-balancer-controller"
+      annotations = {
+        "eks.amazonaws.com/role-arn" = aws_iam_role.lbc.arn
+      }
+    }
+  })]
+
+  depends_on = [aws_iam_role_policy_attachment.lbc_attach]
+}
+
+
+
+
+
+
+
+
+
+
+
--- infra/providers.tf
+++ infra/providers.tf
@@ -17,7 +17,7 @@ provider "kubernetes" {
 # Configure the helm Provider in the same way as the k8s Provider
 # The embedded kubernetes block makes helm use the same eks access token as the k8s provider
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks-cluster.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks-cluster.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
